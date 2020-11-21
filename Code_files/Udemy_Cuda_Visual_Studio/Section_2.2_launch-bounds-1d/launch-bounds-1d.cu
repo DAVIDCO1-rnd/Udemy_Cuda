5c075a990473ab3fc90f8f038b89d28e389024e0
David Cohn <DAVIDCO1@rnd-hub.com>
2020-11-22 00:47:03 +0200

    Updated solution

--- Code_files/Udemy_Cuda_Visual_Studio/Section_2.2_launch-bounds-1d/launch-bounds-1d.cu
+++ Code_files/Udemy_Cuda_Visual_Studio/Section_2.2_launch-bounds-1d/launch-bounds-1d.cu
@@ -16,12 +16,20 @@
 
 __global__ void kernel_1d()
 {
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
-  printf("block %d, thread %d, index %d\n", blockIdx.x, threadIdx.x, index);
+  int index_x = blockIdx.x * blockDim.x + threadIdx.x;
+  int index_y = blockIdx.y * blockDim.y + threadIdx.y;
+  printf("blockIdx.x %d, threadIdx.x %d, index_x %d\n", blockIdx.x, threadIdx.x, index_x);
+  printf("blockIdx.y %d, threadIdx.y %d, index_y %d\n", blockIdx.y, threadIdx.y, index_y);
+  printf("\n");
 }
 
 int main()
 {
-  kernel_1d<<<4, 64>>>();
-  cudaCheckError(cudaDeviceSynchronize());
+  //kernel_1d<<<4, 64>>>();
+  //cudaCheckError(cudaDeviceSynchronize());
+
+	dim3 block_dim(2, 16);
+	dim3 grid_dim(2, 2);
+	kernel_1d<<<grid_dim, block_dim >>>();
+	cudaCheckError(cudaDeviceSynchronize());
 }
