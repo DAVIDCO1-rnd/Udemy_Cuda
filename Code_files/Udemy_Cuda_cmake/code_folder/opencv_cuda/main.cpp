db147d11056490db1faf3e91554b696e74a64333
David Cohn <david.cohn.1234@gmail.com>
2023-07-27 01:38:29 +0300

    updated

--- Code_files/Udemy_Cuda_cmake/code_folder/opencv_cuda/main.cpp
+++ Code_files/Udemy_Cuda_cmake/code_folder/opencv_cuda/main.cpp
@@ -1,12 +1,14 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <string>
 
 #include <iostream>
 
 
-void print_pixels(unsigned char* pixelData, int dimension1, int dimension2)
+void print_pixels(std::string matrix_name, unsigned char* pixelData, int dimension1, int dimension2)
 {
+    printf("%s\n", matrix_name.c_str());
     for (int i = 0; i < dimension1; i++)
     {
         for (int j = 0; j < dimension2; j++)
@@ -17,12 +19,13 @@ void print_pixels(unsigned char* pixelData, int dimension1, int dimension2)
         }
         printf("\n");
     }
+    printf("\n\n");
 }
 
 int main()
 {
     //going back from this folder: ./build/code_folder/Section3.3_spotlights/
-    //std::string image_path = "../../../code_folder/opencv_cuda/images/grayscale.png";
+    std::string image_path = "../../../code_folder/opencv_cuda/images/grayscale.png";
     //cv::Mat image = cv::imread(image_path);
     //if (image.empty())
     //{
@@ -30,32 +33,41 @@ int main()
     //    return 1;
     //}
 
-    const int width1 = 4;
-    const int height1 = 3;
+    const int width3 = 4;
+    const int height3 = 3;
 
-    uchar image_data[height1][width1] = {
+    uchar image_data[height3][width3] = {
        {10, 20, 30, 40},
        {50, 60, 70, 80},
        {90, 100, 110, 120}
     };
 
-    cv::Mat image(height1, width1, CV_8UC1); // 3 rows, 4 columns, 8-bit single-channel (grayscale)
-
-    for (int y = 0; y < image.rows; ++y) {
-        for (int x = 0; x < image.cols; ++x) {
-            image.at<uchar>(y, x) = static_cast<uchar>(image_data[y][x]);
+    cv::Mat image1(height3, width3, CV_8UC1); // 3 rows, 4 columns, 8-bit single-channel (grayscale)
+    for (int y = 0; y < image1.rows; ++y) {
+        for (int x = 0; x < image1.cols; ++x) {
+            image1.at<uchar>(y, x) = static_cast<uchar>(image_data[y][x]);
         }
     }
 
+    cv::imwrite(image_path, image1);
+    cv::Mat image2 = cv::imread(image_path);
 
 
 
 
-    unsigned char* pixelData = image.data;
-    int height = image.rows;
-    int width = image.cols;
 
-    print_pixels(pixelData, height, width);
+
+
+    unsigned char* pixelData1 = image1.data;
+    int height1 = image1.rows;
+    int width1 = image1.cols;
+
+    unsigned char* pixelData2 = image1.data;
+    int height2 = image2.rows;
+    int width2 = image2.cols;
+
+    print_pixels("pixelData1", pixelData1, height1, width1);
+    print_pixels("pixelData2", pixelData2, height2, width2);
 
     //cv::imshow("Display window", image);
     //int k = cv::waitKey(0); // Wait for a keystroke in the window
