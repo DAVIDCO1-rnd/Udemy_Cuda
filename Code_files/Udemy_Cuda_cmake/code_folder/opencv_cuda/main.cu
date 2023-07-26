95ce57c9da78cd19714a5b33c0c4ae8c778d46c5
David Cohn <davidco1@rnd-hub.com>
2023-07-26 18:39:48 +0300

    updated

---
+++ Code_files/Udemy_Cuda_cmake/code_folder/opencv_cuda/main.cu
@@ -0,0 +1,81 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+using namespace cv;
+
+
+int main()
+{
+    //going back from this folder: ./build/code_folder/Section3.3_spotlights/
+    std::string image_path = "../../../code_folder/opencv_cuda/images/00013.jpg";
+    Mat img = imread(image_path);
+
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    if (k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
+
+
+    return 0;
+}
+
+
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include <cuda_runtime.h>
+//
+//// CUDA kernel code
+//__global__ void multiply_by_constant(float* input, float constant, int size)
+//{
+//    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+//    if (idx < size)
+//    {
+//        input[idx] *= constant;
+//    }
+//}
+//
+//int main()
+//{
+//    // Create a sample buffer array in C++
+//    int size = 9;
+//    float input_buffer[] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
+//
+//    // Allocate memory on the GPU
+//    float* d_input_buffer;
+//    cudaMalloc((void**)&d_input_buffer, size * sizeof(float));
+//
+//    // Copy the input buffer from the CPU to the GPU
+//    cudaMemcpy(d_input_buffer, input_buffer, size * sizeof(float), cudaMemcpyHostToDevice);
+//
+//    // Define the block and grid dimensions for CUDA execution
+//    int block_size = 256;
+//    int num_blocks = (size + block_size - 1) / block_size;
+//
+//    // Execute the CUDA kernel
+//    multiply_by_constant<<<num_blocks, block_size>>>(d_input_buffer, 2.0f, size);
+//
+//    // Copy the result back from the GPU to the CPU
+//    float output_buffer[size];
+//    cudaMemcpy(output_buffer, d_input_buffer, size * sizeof(float), cudaMemcpyDeviceToHost);
+//
+//    // Clean up memory on the GPU
+//    cudaFree(d_input_buffer);
+//
+//    // Show the result using OpenCV (just as an example)
+//    cv::Mat result = cv::Mat(1, size, CV_32F, output_buffer);
+//    std::cout << "Input Buffer: " << cv::Mat(1, size, CV_32F, input_buffer) << std::endl;
+//    std::cout << "Output Buffer: " << result << std::endl;
+//
+//    return 0;
+//}
