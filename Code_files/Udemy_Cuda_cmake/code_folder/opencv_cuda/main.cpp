d8b37b59bae076ea17d9b5566a27784ca62dd067
David Cohn <davidco1@rnd-hub.com>
2023-08-01 10:31:49 +0300

    updated

--- Code_files/Udemy_Cuda_cmake/code_folder/opencv_cuda/main.cpp
+++ Code_files/Udemy_Cuda_cmake/code_folder/opencv_cuda/main.cpp
@@ -15,7 +15,7 @@
 
 bool read_image_from_file = false;
 const int height = 3;
-const int width = 6;
+const int width = 5;
 
 enum class DirectionOfRotation {
     Clockwise,
@@ -28,32 +28,63 @@ enum class PixelType {
     FLOAT = 4
 };
 
-void print_pixels(std::string matrix_name, unsigned char* pixelData, int dimension1, int dimension2, PixelType pixel_type)
+void print_single_val(unsigned char* pixelData, int i, PixelType pixel_type)
+{
+    if (pixel_type == PixelType::UCHAR)
+    {
+        unsigned char current_val = pixelData[i];
+        printf("0x%04x, ", current_val);
+    }
+    if (pixel_type == PixelType::USHORT)
+    {
+        unsigned char sub_pixel1 = pixelData[i];
+        unsigned char sub_pixel2 = pixelData[i + 1];
+        unsigned short current_val = 0x100 * sub_pixel2 + sub_pixel1;
+        printf("0x%04x, ", current_val);
+    }
+    if (pixel_type == PixelType::FLOAT)
+    {
+        unsigned char sub_pixel1 = pixelData[i];
+        unsigned char sub_pixel2 = pixelData[i + 1];
+        unsigned char sub_pixel3 = pixelData[i + 2];
+        unsigned char sub_pixel4 = pixelData[i + 3];
+        unsigned short current_val = 0x1000000 * sub_pixel4 + 0x10000 * sub_pixel3 + 0x100 * sub_pixel2 + sub_pixel1;
+        printf("0x%04x, ", current_val);
+    }
+}
+
+
+void print_pixels_1D(std::string matrix_name, unsigned char* pixelData, int dimension1, int dimension2, PixelType pixel_type)
 {
     int pixel_size = (int)pixel_type;
-    printf("%s\n", matrix_name.c_str());
+    printf("%s as 1D array\n", matrix_name.c_str());
+    for (int i = 0; i < pixel_size * dimension1 * dimension2; i+=pixel_size)
+    {
+        print_single_val(pixelData, i, pixel_type);
+    }
+    printf("\n\n");
+}
+
+void print_pixels_2D(std::string matrix_name, unsigned char* pixelData, int dimension1, int dimension2, PixelType pixel_type)
+{
+    int pixel_size = (int)pixel_type;
+    printf("%s as 2D array\n", matrix_name.c_str());
     for (int i = 0; i < dimension1; i++)
     {
-        for (int j = 0; j < pixel_size * dimension2; j+= pixel_size)
+        for (int j = 0; j < pixel_size * dimension2; j += pixel_size)
         {
             int current_index = i * pixel_size * dimension2 + j;
-            if (pixel_type == PixelType::UCHAR)
-            {
-                unsigned char current_val = pixelData[current_index];
-                printf("0x%04x, ", current_val);
-            }
-            if (pixel_type == PixelType::USHORT)
-            {
-                unsigned char sub_pixel1 = pixelData[current_index];
-                unsigned char sub_pixel2 = pixelData[current_index + 1];
-                unsigned short current_val = 0x100 * sub_pixel2 + sub_pixel1;
-                printf("0x%04x, ", current_val);
-            }
-
+            print_single_val(pixelData, current_index, pixel_type);
         }
         printf("\n");
     }
     printf("\n\n");
+}
+
+void print_pixels(std::string matrix_name, unsigned char* pixelData, int dimension1, int dimension2, PixelType pixel_type)
+{
+    print_pixels_1D(matrix_name, pixelData, dimension1, dimension2, pixel_type);
+    print_pixels_2D(matrix_name, pixelData, dimension1, dimension2, pixel_type);
 }
 
 #ifdef USE_CUDA
@@ -201,10 +232,16 @@ int main()
 
     if (read_image_from_file == false)
     {
+        //uchar image_data[height][width] = {
+        //   {0x05, 0x10, 0x15, 0x20, 0x25, 0x30},
+        //   {0x35, 0x40, 0x45, 0x50, 0x55, 0x60},
+        //   {0x65, 0x70, 0x75, 0x80, 0x85, 0x90}
+        //};
+
         uchar image_data[height][width] = {
-           {0x05, 0x10, 0x15, 0x20, 0x25, 0x30},
-           {0x35, 0x40, 0x45, 0x50, 0x55, 0x60},
-           {0x65, 0x70, 0x75, 0x80, 0x85, 0x90}
+           {0x05, 0x10, 0x15, 0x20, 0x25},
+           {0x30, 0x35, 0x40, 0x45, 0x50},
+           {0x55, 0x60, 0x65, 0x70, 0x75}
         };
         image1_uchar = build_image_from_data(image_data, PixelType::UCHAR);        
         print_pixels("built-in image1_uchar", image1_uchar.data, image1_uchar.rows, image1_uchar.cols, PixelType::UCHAR);
