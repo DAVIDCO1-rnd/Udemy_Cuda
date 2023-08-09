7f0bf06cb2bd94a192ae61413bd09914d1e6fdd6
David Cohn <davidco1@rnd-hub.com>
2023-08-09 18:42:47 +0300

    updated

---
+++ Code_files/Udemy_Cuda_cmake/code_folder/opencv_cuda_006_threshold_operator/src/Inverse.cu
@@ -0,0 +1,59 @@
+// Nvcc predefines the macro __CUDACC__.
+// This macro can be used in sources to test whether they are currently being compiled by nvcc.
+#ifndef __CUDACC__
+#error Must be compiled with CUDA compiler!
+#endif
+
+#include <stdio.h>
+//#include <cutil.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+#include "CudaMain.cuh"
+#include "Inverse.cuh"
+
+#include "Inverse_curnel.cu"
+
+__wchar_t* Inverse(void* deviceInputBuffer, void* deviceOutputBuffer,
+	int subPixelType, int maxValue, int alphaChannelNumber, int pixelSize, int channelSize,
+	int widthImage, int heightImage, int strideSourceImage, int strideResultImage,
+	int blockSizeX, int blockSizeY, int blockSizeZ,
+	int gridSizeX, int gridSizeY)
+{
+	dim3 blockDim(blockSizeX, blockSizeY, blockSizeZ); // block size = number of threads
+	dim3 gridDim(gridSizeX, gridSizeY); // grid size = number of blocks
+
+	switch (subPixelType)
+	{
+	case 1: // 8bit (0...0xff)
+	{
+		InvertImageKernel<unsigned char> << < gridDim, blockDim >> >
+			((unsigned char*)deviceInputBuffer, (unsigned char*)deviceOutputBuffer,
+				maxValue, alphaChannelNumber, pixelSize, channelSize,
+				widthImage, heightImage, strideSourceImage, strideResultImage);
+		break;
+	}
+	case 2: // 16bit (0...0xffff)
+	{
+		InvertImageKernel<unsigned short> << < gridDim, blockDim >> >
+			((unsigned char*)deviceInputBuffer, (unsigned char*)deviceOutputBuffer,
+				maxValue, alphaChannelNumber, pixelSize, channelSize,
+				widthImage, heightImage, strideSourceImage, strideResultImage);
+		break;
+	}
+	case 4: // Float (0...1)
+	{
+		InvertImageKernel<float> << < gridDim, blockDim >> >
+			((unsigned char*)deviceInputBuffer, (unsigned char*)deviceOutputBuffer,
+				(float)maxValue, alphaChannelNumber, pixelSize, channelSize,
+				widthImage, heightImage, strideSourceImage, strideResultImage);
+		break;
+	}
+	}
+
+	cudaError_t err = cudaThreadSynchronize();
+	if (err != 0)
+		return CudaErrorToErrorMessage(err);
+
+	return NULL;
+}
